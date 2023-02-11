8f393df6b75b2edd5ed12d1ebd831251da437a2a
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2023-02-11 14:17:56 +0200

    add: Initial data models

---
+++ ExamCreatorOnline/ExamCreatorOnline.Data/Models/Exam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamCreatorOnline.Data.Models
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int AveragePoints { get; set; }
+
+        public int GoodPoints { get; set; }
+
+        public int VeryGoodPoints { get; set; }
+
+        public int ExcelentPoints { get; set; }
+    }
+}
